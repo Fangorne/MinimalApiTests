cdf37ea055f22fd5d9b89b473a7b641a874ad519
Fangorne <marcus_the@msn.com>
2022-11-03 11:14:40 +0100

    Add project files.

---
+++ MinimalApiTests.Structured/Program.cs
@@ -0,0 +1,9 @@
+﻿using MinimalApiTests.Structured;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointDefinitions(typeof(IEndpointDefinition));
+
+var app = builder.Build();
+app.UseEndpointDefinitions();
+
+app.Run();
---
+++ MinimalApiTests.Structured/Services/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using MinimalApiTests.Structured.Models;
+
+namespace MinimalApiTests.Structured.Services;
+
+public interface ICustomerService
+{
+    void Create(Customer? customer);
+
+    Customer? GetById(Guid id);
+
+    List<Customer> GetAll();
+
+    void Update(Customer customer);
+
+    void Delete(Guid id);
+}
